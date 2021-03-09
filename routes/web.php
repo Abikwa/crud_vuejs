51c4f68a065f4b788de550099a498b4b1ae96ae7
Abikwa <Abikalex8@gmail.com>
2021-03-09 13:35:56 +0200

    update routing

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home/index');
-});
+
 
 Route::get('{any}', function () {
-    return view('app');
+    return view('templates/default');
 })->where('any', '.*');
