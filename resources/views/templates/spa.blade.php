9f85ddbea9ccffba90ab74808425b6b0a661279a
Abikwa <Abikalex8@gmail.com>
2021-03-10 14:25:37 +0200

    update spa template

--- resources/views/templates/spa.blade.php
+++ resources/views/templates/spa.blade.php
@@ -14,6 +14,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
     <script src="{{ asset('js/dashboard.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
     @yield('script')
 </body>
 
