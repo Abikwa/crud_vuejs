1ad31a5d8097cebd8bfc0c07e188959732ebf634
Abikwa <Abikalex8@gmail.com>
2021-03-12 16:35:51 +0200

    update layout app

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -4,8 +4,8 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="collapse navbar-collapse">
                 <div class="navbar-nav">
-                    <router-link to="/" class="nav-item nav-link">Products List</router-link>
-                    <router-link to="/create" class="nav-item nav-link">Create Product</router-link>
+                    <router-link to="/marks" class="nav-item nav-link">Marks</router-link>
+                    <router-link to="/marks/create" class="nav-item nav-link">New Mark</router-link>
                 </div>
             </div>
         </nav>
