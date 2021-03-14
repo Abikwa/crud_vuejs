f72a54380364dc8d13d0b819d18a57166621d07c
Abikwa <Abikalex8@gmail.com>
2021-03-14 12:48:07 +0200

    update edit marks components

---
+++ resources/js/components/marks/Edit.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h3 class="text-center">Edit mark</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <form @submit.prevent="updatemark">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" v-model="mark.name">
+                    </div>
+                    <div class="form-group">
+                        <label>Detail</label>
+                        <input type="text" class="form-control" v-model="mark.detail">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+ 
+<script>
+    export default {
+        data() {
+            return {
+                mark: {}
+            }
+        },
+        created() {
+            this.axios
+                .get(`http://localhost:8000/api/marks/${this.$route.params.id}`)
+                .then((res) => {
+                    this.mark = res.data;
+                });
+        },
+        methods: {
+            updatemark() {
+                this.axios
+                    .patch(`http://localhost:8000/api/marks/${this.$route.params.id}`, this.mark)
+                    .then((res) => {
+                        this.$router.push({ name: 'marks' });
+                    });
+            }
+        }
+    }
+</script>
