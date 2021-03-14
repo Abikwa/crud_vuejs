ff2c7971b0cac56369c7c76b92ba4c4d621618bb
Abikwa <Abikalex8@gmail.com>
2021-03-14 13:34:15 +0200

    update component marks create

---
+++ resources/js/components/marks/Create.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h3 class="text-center">Create mark</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <form @submit.prevent="addmark">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" v-model="mark.name">
+                    </div>
+                    <div class="form-group">
+                        <label>Detail</label>
+                        <input type="text" class="form-control" v-model="mark.detail">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+ 
+<script>
+    export default {
+        data() {
+            return {
+                mark: {}
+            }
+        },
+        methods: {
+            addmark() {
+                this.axios
+                    .post('http://localhost:8000/api/marks', this.mark)
+                    .then(response => (
+                        this.$router.push({ name: 'marks' })
+                    ))
+                    .catch(err => console.log(err))
+                    .finally(() => this.loading = false)
+            }
+        }
+    }
+</script>
